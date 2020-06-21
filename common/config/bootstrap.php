4ac5df58189f7c33251eaafccaad3b5ea7f481d7
vladlay <wlawallay@gmail.com>
2020-06-21 14:22:01 +0300

    Update bootstrap config

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,4 +3,6 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-Yii::setAlias('@uploads', dirname(dirname(__DIR__)) . '/frontend/web/uploads');
+// Yii::setAlias('@uploads', dirname(dirname(__DIR__)) . '/frontend/web/uploads');
+// /home/wlawallay/projects/diploma.net/frontend/web/uploads/btc.png
+Yii::setAlias('@uploads', 'http://diploma.net/uploads');
