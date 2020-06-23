7b8f7aca1b286faa51cbdea16317d519c1b8122c
vladlay <wlawallay@gmail.com>
2020-06-23 23:16:44 +0300

    Add FontAwesome

---
+++ backend/assets/FontAwesomeAsset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+class FontAwesomeAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/fortawesome/font-awesome';
+    public $css = [
+        'css/all.min.css',
+    ];
+}
