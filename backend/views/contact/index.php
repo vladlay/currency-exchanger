d44bf110ff1f25401b03180f68c3381ce56aeb39
vladlay <wlawallay@gmail.com>
2020-06-23 23:36:14 +0300

    Add new controller and views

---
+++ backend/views/contact/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\ContactSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Contacts';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="contact-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Contact', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'value',
+            'icon',
+
+            ['class' => 'common\classes\MyActionColumn'],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
