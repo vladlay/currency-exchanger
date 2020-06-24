d3655497bbbcc2db678bc691bf0e13826a4de91c
vladlay <wlawallay@gmail.com>
2020-06-24 10:38:45 +0300

    Add review views

---
+++ frontend/views/review/view.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Review */
+
+$this->title = 'Ваш отзыв';
+$this->params['breadcrumbs'][] = ['label' => 'Reviews', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="review-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Редактировать', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Удалить', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            [
+                'attribute' => 'created_at',
+                'value' => date('Y-m-d H:i:s', $model->created_at),
+            ],
+            
+            'created_by',
+            'text',
+        ],
+    ]) ?>
+
+</div>
